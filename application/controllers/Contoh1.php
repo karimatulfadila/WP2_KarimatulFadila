ba04025c6f39ab592128abd86fe99db0ad1ac3e2
karimatulfadila <kharimatulfadillah25@gmail.com>
2021-09-25 00:33:49 +0700

    Upload awal project

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,19 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Personal Data!</h1>";
+		echo "Hai! Perkenalkan, <br>"; 
+		echo "Nama : Karimatul Fadila <br>";
+		echo "Tempat, Tanggal Lahir : Jakarta, 25 Mei 2002 <br>";
+		echo "No. Telepon : 082112328197 <br>";
+		echo "Alamat : Jl. Kapten yusuf RT.002/RW.002 N0 51 Kecamatan Ciomas Kab. Bogor <br>";
+		echo "NIM : 12200174 <br>";
+		echo "Kelas : 12.3A.13 <br>";
+		echo "Program Studi : Sistem Informasi <br>";
+		echo "Fakultas	: Teknik & Informatika <br>";
+		echo "Pendidikan : Universitas Bina Sarana Informatika <br>";
+
+    }
+}

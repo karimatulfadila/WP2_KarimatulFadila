fb66d29a33ba29fa0942bc8cefba16d60acb0630
karimatulfadila <kharimatulfadillah25@gmail.com>
2021-10-07 22:11:30 +0700

    MEMBUAT TEMPLATE (TEMPLATING) WEB SEDERHANA

--- application/views/v-about.php
+++ application/views/v-about.php
@@ -1,22 +1,24 @@
-<section>     
-    <h1><?php echo $judul ?></h1> 
-    <h4>Nama</h4>     
-    <ul type="disc">        
-         <li>Nama Depan : Imam</li>         
-         <li>Nama Belakang : Nawawi</li>    
-         </ul>    
-          <br>    
-           <h4>Alamat</h4>     
-           <ul type="none">         
-               <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>     
-            </ul>          
-            <h4>Tempat Lahir</h4>     
-            <ul type="none">         
-                <li>Cirebon</li>    
-             </ul>  
-    <h4>Olah Raga Favorit</h4>     
-    <ul type="square">         
-        <li>Bulutangkis</li>         
-    <li>Catur</li>     
-</ul> 
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Karimatul</li>
+        <li>Nama Belakang : Fadila</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>JL. Kapten yusuf kotabatu no.51 rt 002/002 Kab. Bogor</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Hobby</h4>
+    <ul type="squre">
+        <li>Cooking Cake</li>
+        <li>Master Of Ceremony</li>
+    </ul>
 </section>
--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">     
         <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>     
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() 
-        ?> assets/css/stylebuku.css"> 
+         ?>assets/css/stylebuku.css"> 
         </head> 
         <body>     
             <div id="wrapper">         
@@ -15,10 +15,8 @@
                     </hgroup>             
                     <nav>                 
                         <ul>                     
-                            <li><a href="<?php echo 
-                            base_url().'index.php/web' ?>">Home</a></li>                     
-                            <li><a href=”<?php echo 
-                            base_url().'index.php/web/about' ?>">About </a></li>                 
+                            <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>                     
+                            <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>                 
                             </ul>            
                          </nav>            
                           <div class="clear"></div>         
